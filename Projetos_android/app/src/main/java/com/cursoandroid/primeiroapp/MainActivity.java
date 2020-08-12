0ca396eac998f17da3c7bae8da0a80abf20241f9
Filipe Barbosa <filipebe.barbosa@hotmail.com>
2020-08-12 11:05:16 -0300

    primeiro commit

---
+++ Projetos_android/app/src/main/java/com/cursoandroid/primeiroapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cursoandroid.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
