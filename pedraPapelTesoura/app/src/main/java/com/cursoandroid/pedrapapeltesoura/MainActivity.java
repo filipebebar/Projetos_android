be22b54562573d61bafc075d213e40d5efcda42e
Filipe Barbosa <filipebe.barbosa@hotmail.com>
2020-10-01 11:10:54 -0300

    primeiro comiit app jogo pedra papel tesoura

---
+++ pedraPapelTesoura/app/src/main/java/com/cursoandroid/pedrapapeltesoura/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cursoandroid.pedrapapeltesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void opcaoSelecionada(String opcapSelecionada){
+
+    }
+}
