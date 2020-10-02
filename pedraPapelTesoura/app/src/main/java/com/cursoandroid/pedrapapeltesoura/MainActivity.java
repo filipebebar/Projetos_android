21ae5687aa75ea94816342c3b08b0f72402c1936
Filipe Barbosa <filipebe.barbosa@hotmail.com>
2020-10-02 10:16:29 -0300

    Adicionando as opções de seleção

--- pedraPapelTesoura/app/src/main/java/com/cursoandroid/pedrapapeltesoura/MainActivity.java
+++ pedraPapelTesoura/app/src/main/java/com/cursoandroid/pedrapapeltesoura/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -16,27 +17,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void selecionadoPedra(View view){
+    public void selecionadoPedra(View view) {
         this.opcaoSelecionada("pedra");
     }
 
-    public void selecionadoPapel(View view){
+    public void selecionadoPapel(View view) {
         this.opcaoSelecionada("pedra");
     }
 
-    public void selecionadoTesoura(View view){
+    public void selecionadoTesoura(View view) {
         this.opcaoSelecionada("pedra");
     }
 
-    public void opcaoSelecionada(String opcapSelecionada){
+    public void opcaoSelecionada(String escolhaUsuario) {
 
         ImageView imageResultado = findViewById(R.id.imageResultado);
+        TextView textoResultado = findViewById(R.id.textResultado);
 
         int numeroAleatorio = new Random().nextInt(3);
-        String[] opcoes = { "pedra", "papel","tesoura"};
-        String opcaoApp = opcoes[numeroAleatorio];
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String escolhaApp = opcoes[numeroAleatorio];
 
-        switch (opcaoApp){
+        switch (escolhaApp) {
             case "pedra":
                 imageResultado.setImageResource(R.drawable.pedra);
                 break;
@@ -46,13 +48,24 @@ public class MainActivity extends AppCompatActivity {
             case "tesoura":
                 imageResultado.setImageResource(R.drawable.tesoura);
                 break;
-
-
-
-
-
-
         }
+        if (
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+                (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+                (escolhaApp == "pedra" && escolhaUsuario == "tesoura")
+
+        ) {//app ganhador
+            textoResultado.setText("Você Perdeu");
+        } else if (
+                (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+                (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
+                (escolhaUsuario == "pedra" && escolhaApp == "tesoura")
+        ) {//usuario ganhador
+            textoResultado.setText("Você Ganhou!");
+        } else {//empate
+            textoResultado.setText("Empatamos");
+        }
+
 
     }
 }
